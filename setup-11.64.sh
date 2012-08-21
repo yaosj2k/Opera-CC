ad0b8ac9e494f5fce97343b284fe931a648bdc98
atmouse- <at86mouse@gmail.com>
2012-08-21 22:52:51 +0800

    new setup

---
+++ setup-11.64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SH_COM=$(cd "${0%/*}";pwd)/$(basename $0)
+
+readlink $SH_COM >/dev/null 2>&1
+if [ $? -eq 0 ];then
+    R_SH_COM=$(readlink $SH_COM)
+    BASE_DIR=${R_SH_COM%/*}
+    cd $BASE_DIR
+else 
+    BASE_DIR=${SH_COM%/*}
+    cd $BASE_DIR
+  fi
+
+version="opera-11.64-1403.x86_64.linux"
+wget ftp://ftp.opera.com/pub/opera/linux/1164/opera-11.64-1403.x86_64.linux.tar.xz
+
+tar xf ${version}.tar.xz -C ./ --exclude=${version}/opera
+mv ${version}/* ./
+rmdir ${version}
+./bin/parse.sh
+
+rm opera-11.64-1403.x86_64.linux.tar.xz
