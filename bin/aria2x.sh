3b321495b404fab8b16b5c28cc8e83110b5550eb
atmouse- <at86mouse@gmail.com>
2012-08-26 01:03:36 +0800

    new filesave method

--- bin/aria2x.sh
+++ bin/aria2x.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
-aria2c --load-cookies=/tmp/cookies.txt -c $1 -d $HOME/Downloads -s5 -x5 --referer=$2
+
+DOWN_URL="$1"
+REF="$2"
+COOKIES="/tmp/cookies.txt"
+# get filename from the url and convert some hex codes
+# i hate spaces in filenames so i'm switching them
+# with underscores here, adjust the first s///g if
+# you want to keep the spaces
+FILE="$(basename $DOWN_URL | sed -r \
+'s/[_%]20/\_/g;s/[_%]22/\"/g;s/[_%]23/\#/g;s/[_%]24/\$/g;
+s/[_%]25/\%/g;s/[_%]26/\&/g;s/[_%]28/\(/g;s/[_%]29/\)/g;
+s/[_%]2C/\,/g;s/[_%]2D/\-/g;s/[_%]2E/\./g;s/[_%]2F/\//g;
+s/[_%]3C/\</g;s/[_%]3D/\=/g;s/[_%]3E/\>/g;s/[_%]3F/\?/g;
+s/[_%]40/\@/g;s/[_%]5B/\[/g;s/[_%]5C/\\/g;s/[_%]5D/\]/g;
+s/[_%]5E/\^/g;s/[_%]5F/\_/g;s/[_%]60/\`/g;s/[_%]7B/\{/g;
+s/[_%]7C/\|/g;s/[_%]7D/\}/g;s/[_%]7E/\~/g;s/[_%]2B/\+/g')"
+
+UA="Opera/9.80 (X11; Linux x86_64; U; en) Presto/2.10.229 Version/11.64"
+DIRFILE=$(zenity --file-selection --save --filename="$FILE" --confirm-overwrite)
+DIRNAME="$(dirname $DIRFILE)"
+FILENAME="$(basename $DIRFILE)"
+
+echo "save to ${DIRFILE}"
+aria2c --allow-overwrite=true \
+    --user-agent="$UA" \
+    --load-cookies="$COOKIES" \
+    -d "$DIRNAME" \
+    -o "$FILENAME" \
+    -c "$DOWN_URL" \
+    -s5 -x5 \
+    --referer="$REF"
 read -p "Press Enter to Continue "
--- profile/script/@dl_lixianxunlei.js
+++ profile/script/@dl_lixianxunlei.js
@@ -2,8 +2,8 @@
 // @name insertDL
 // @description xunDL
 // @create 2012-4-10
-// @lastmodified 2012-4-10
-// @version 1.0.0.0
+// @lastmodified 2012-8-26
+// @version 1.0.0.1
 // @include http://dynamic.cloud.vip.xunlei.com/*
 // ==/UserScript==
 
@@ -31,7 +31,7 @@
                 var dl_button=document.createElement('a');
                 var a_text=document.createTextNode('▲build DL');
                 //dl_button.setAttribute('onclick',"javascript:var click_area=document.createElement('li');click_area.setAttribute('align','center');var click_button=document.createElement('a');var a_text=document.createTextNode('▲download');click_button.setAttribute('href','dl:'+'"+url+"'+','+window.location.href+','+document.cookie);click_button.appendChild(a_text);click_area.appendChild(click_button);document.querySelector('#main_nav').children[0].appendChild(click_area);");
-                dl_button.setAttribute('onclick',"javascript:document.getElementById('insert_url').href='dl:'+'"+url+"'+','+window.location.href+','+document.cookie");
+                dl_button.onclick="javascript:document.getElementById('insert_url').href='"+url+"'";
                 dl_button.appendChild(a_text);
                 dl_url[i].parentNode.appendChild(dl_button);
             }
@@ -67,7 +67,7 @@
             if(url){
                 var dl_button=document.createElement('a');
                 var a_text=document.createTextNode('▲build DL');
-                dl_button.setAttribute('onclick',"javascript:document.getElementById('insertbt_url').href='dl:'+'"+url+"'+','+window.location.href+','+document.cookie");
+                dl_button.onclick="javascript:document.getElementById('insert_url').href='"+url+"'";
                 dl_button.appendChild(a_text);
                 btdown_url[i].parentNode.insertBefore(dl_button,btdown_url[i]);
             }
