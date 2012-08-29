a27d36a312fb24802d300a30648af4109c3c51a7
atmouse- <at86mouse@gmail.com>
2012-08-29 13:07:30 +0800

    fix check sys platform

--- setup-11.64.sh
+++ setup-11.64.sh
@@ -10,9 +10,16 @@ if [ $? -eq 0 ];then
 else 
     BASE_DIR=${SH_COM%/*}
     cd $BASE_DIR
-  fi
+fi
 
-version="opera-11.64-1403.x86_64.linux"
+SYS_BIT=$(getconf LONG_BIT)
+
+if [[ $SYS_BIT == '64' ]] ;then
+    version="opera-11.64-1403.x86_64.linux"
+else
+    version="opera-11.64-1403.i386.linux"
+fi
+
 wget ftp://ftp.opera.com/pub/opera/linux/1164/${version}.tar.xz
 
 tar xf ${version}.tar.xz -C ./ --exclude=${version}/opera
