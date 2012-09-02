68b47a86847dd20ce8e3692f9c3c7ec66e5120cc
atmouse- <at86mouse@gmail.com>
2012-09-03 00:44:34 +0800

    merge squid i386

--- setup-11.64.sh
+++ setup-11.64.sh
@@ -16,8 +16,12 @@ SYS_BIT=$(getconf LONG_BIT)
 
 if [[ $SYS_BIT == '64' ]] ;then
     version="opera-11.64-1403.x86_64.linux"
+    squid_link="https://dl.dropbox.com/sh/m2hz4s4hvd2g28x/NhKeU9kS8D/squid-x86_64.tar.gz"
+    platform="x86_64"
 else
     version="opera-11.64-1403.i386.linux"
+    squid_link="https://dl.dropbox.com/sh/m2hz4s4hvd2g28x/nUPMR9xe9f/squid-i386.tar.gz"
+    platform="i386"
 fi
 
 wget ftp://ftp.opera.com/pub/opera/linux/1164/${version}.tar.xz
@@ -25,6 +29,13 @@ wget ftp://ftp.opera.com/pub/opera/linux/1164/${version}.tar.xz
 tar xf ${version}.tar.xz -C ./ --exclude=${version}/opera
 mv ${version}/* ./
 rmdir ${version}
+
+### squid
+wget ${squid_link}
+tar -xzvf squid-${platform}.tar.gz -C ./bin/squid/
+rm -rf squid-${platform}.tar.gz
+
+./bin/update-drf.sh
 
 chmod -R 755 *
 ./bin/parse.sh
